73ea4d3f9ce2528b84763477592af9e26e08380f
Giuseppe Varriale <pepe.saoleo@gmail.com>
2020-05-12 18:04:36 -0300

    add some javascript to form

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-alert("teste");
+var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
+
+document.getElementById("form-user-create").addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    fields.forEach(function (field, index) {
+
+        if (field.name == "gender") {
+
+            if (field.checked) {
+                user[field.name] = field.value;
+            }
+
+        } else {
+
+            user[field.name] = field.value;
+        }
+
+    });
+    addLine(user);
+    console.log("ok");
+})
