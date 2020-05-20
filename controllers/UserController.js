ef49623776000480c1fb220c93023539edc3d556
Giuseppe Varriale <pepe.saoleo@gmail.com>
2020-05-20 17:27:07 -0300

    Refact- Make Tr template better using methods

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -41,27 +41,14 @@ class UserController {
             result._photo = content;
           }
           result._register = new Date(userOld._register);
+          
           this.updateUser(tr, result);
 
-          tr.dataset.user = JSON.stringify(result);
+          let user = new User();
+          user.loadFromJSON(result);
+          
+          this.getTr(user, tr);
 
-          tr.innerHTML = `
-          <tr>
-            <td><img src="${result._photo}" alt="User Image" 
-                class="img-circle img-sm"></td>
-            <td>${result._name}</td>
-            <td>${result._email}</td>
-            <td>${result._admin ? "Sim" : "Não"}</td>
-            <td>${Utils.dateFormat(result._register)}</td>
-            <td>
-            <button type="button" class="btn btn-primary btn-edit btn-xs 
-                btn-flat">Editar</button>
-            <button type="button" class="btn btn-danger btn-delete btn-xs 
-                btn-flat">Excluir</button>
-            </td>
-          </tr>
-        `;
-          this.addEventsTr(tr);
           this.updateCount();
           this.formUpdateEl.reset();
 
@@ -181,8 +168,8 @@ class UserController {
     let users = [];
     if (localStorage.getItem("users")) {
       try {
-      users = JSON.parse(localStorage.getItem("users"));}
-      catch (e){
+        users = JSON.parse(localStorage.getItem("users"));
+      } catch (e) {
         localStorage.clear();
       }
     }
@@ -209,7 +196,7 @@ class UserController {
     localStorage.setItem("users", JSON.stringify(users));
   }
 
-  updateUser(tr, userData){
+  updateUser(tr, userData) {
     let users = this.getUsersStorage();
     users[tr.sectionRowIndex] = userData;
     localStorage.setItem("users", JSON.stringify(users));
@@ -222,9 +209,17 @@ class UserController {
   }
 
   addLine(dataUser) {
-    let tr = document.createElement("tr");
+    let tr = this.getTr(dataUser);
+    this.tableEl.appendChild(tr);
+
+    this.updateCount();
+  }
+
+  getTr(dataUser, tr = null) {
+    if (tr == null) tr = document.createElement("tr");
 
     tr.dataset.user = JSON.stringify(dataUser);
+    
     tr.innerHTML = `
       <tr>
         <td><img src="${dataUser.photo}" alt="User Image" 
@@ -243,10 +238,7 @@ class UserController {
     `;
 
     this.addEventsTr(tr);
-
-    this.tableEl.appendChild(tr);
-
-    this.updateCount();
+    return tr;
   }
 
   addEventsTr(tr) {
